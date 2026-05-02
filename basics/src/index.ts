726dbcda2d30bdff224368640049446d0f0e9e13
Amir Mustafa <amirengg15@gmail.com>
2026-05-02 17:40:52 +0530

    feat: convert js to ts

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -17,6 +17,14 @@ async function run() {
         role: "user",
         content: `${userInput} Answer in max 2 lines. Keep answers short and concise`,
       },
+      //   {
+      //     role: "system",
+      //     content: `You are a helpful assistant for learning AI with Node and Python. Provide concise and informative answers to user questions about AI, Node.js, and Python. Keep responses short and to the point.`,
+      //   },
+      //   {
+      //     role: "assistant",
+      //     content: `Sure! To learn AI with Node and Python, start with online courses on platforms like Coursera or Udemy. Practice coding AI projects using libraries like TensorFlow.js for Node and TensorFlow or PyTorch for Python.`,
+      //   },
     ],
     max_completion_tokens: +process.env.MAX_COMPLETION_TOKENS || 50, // ✅ biggest cost control
     temperature: +process.env.TEMPERATURE || 0.5, // ✅ stable (less randomness = less tokens sometimes)
