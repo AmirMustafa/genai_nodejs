b5fa7903eeb852ab8391daaa7926cb55f38f102d
Amir Mustafa <amirengg15@gmail.com>
2026-05-09 08:54:03 +0530

    fix: terminal breaking after 1st open ai response

--- chat_app/src/index.ts
+++ chat_app/src/index.ts
@@ -10,20 +10,28 @@ const openai = new OpenAI({
 
 async function run() {
   const input = promptSync({ sigint: true });
-  const userInput = input("Enter your message: ");
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-5.4-mini",
-    messages: [
-      {
-        role: "user",
-        content: `${userInput} Answer in max 2 lines. Keep answers short and concise`,
-      },
-    ],
-  });
-  console.log(
-    `Response: ${response.choices[0]?.message?.content ?? "No Response"}`,
-  );
+  while (true) {
+    const userInput = input("") as string;
+
+    if (userInput.toLowerCase() === "exit") {
+      console.log("Exiting chat");
+      break;
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-5.4-mini",
+      messages: [
+        {
+          role: "user",
+          content: `${userInput} Answer in max 2 lines. Keep answers short and concise`,
+        },
+      ],
+    });
+    console.log(
+      `Response: ${response.choices[0]?.message?.content ?? "No Response"}`,
+    );
+  }
 }
 
 run();
