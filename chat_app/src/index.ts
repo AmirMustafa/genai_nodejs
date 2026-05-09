4af416ba8ae7b1a1d0095e30841d60813e462937
Amir Mustafa <amirengg15@gmail.com>
2026-05-09 08:44:35 +0530

    feat: interacting with Open AI | Gen AI

--- chat_app/src/index.ts
+++ chat_app/src/index.ts
@@ -1,2 +1,29 @@
-let content: string = "Gen AI 2026 course with Node.js and Python";
-console.log(content);
+import { OpenAI } from "openai";
+import dotenv from "dotenv";
+import promptSync from "prompt-sync";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function run() {
+  const input = promptSync({ sigint: true });
+  const userInput = input("Enter your message: ");
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-5.4-mini",
+    messages: [
+      {
+        role: "user",
+        content: `${userInput} Answer in max 2 lines. Keep answers short and concise`,
+      },
+    ],
+  });
+  console.log(
+    `Response: ${response.choices[0]?.message?.content ?? "No Response"}`,
+  );
+}
+
+run();
