e3db8160d20a427c8d21a2e4be5ce007cd288fb0
Amir Mustafa <amirengg15@gmail.com>
2026-05-16 15:25:38 +0530

    feat: tools - applying tools code in ai response

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -16,18 +16,60 @@ const context: OpenAI.ChatCompletionMessageParam[] = [
   },
   {
     role: "user",
-    content: "What is the current time in UAE?",
+    content: "What is the current time in New York?",
   },
 ];
+
+// Creating out own tool / function to get the current time in New York
+function getTimeInNewYork() {
+  return new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
+}
 
 async function callOpenAITools() {
   const response = await openai.chat.completions.create({
     model: "gpt-5.4-mini",
     messages: context,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "getTimeInNewYork",
+          description: "Get the current time in New York",
+        },
+      },
+    ],
+    tool_choice: "auto", // Let the model decide when to call the tool
   });
 
-  const responseMessage = response.choices[0]?.message;
+  // step 2 - check if the model wants to call the tool
+  const willInvokeTool = response.choices[0]?.finish_reason === "tool_calls";
+  const toolCall = response.choices[0]?.message.tool_calls?.[0];
 
+  if (willInvokeTool && toolCall) {
+    const toolName = "function" in toolCall ? toolCall.function.name : "";
+
+    if (toolName === "getTimeInNewYork") {
+      const time = getTimeInNewYork();
+      const responseMessage = response.choices[0]?.message;
+
+      if (responseMessage) {
+        context.push(responseMessage);
+      }
+
+      context.push({
+        role: "tool",
+        content: time,
+        tool_call_id: toolCall?.id ?? "",
+      });
+    }
+  }
+
+  const secondResponse = await openai.chat.completions.create({
+    model: "gpt-5.4-mini",
+    messages: context,
+  });
+
+  const responseMessage = secondResponse.choices[0]?.message;
   console.log(responseMessage?.content);
 }
 
